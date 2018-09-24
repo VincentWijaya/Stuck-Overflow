e2067afd9d18c5d0393727a36a85962bdcbe1a87
Vincent Wijaya <weqqss@yahoo.com>
2018-09-24 14:47:05 +0700

    Finished CRUD question, upvote question and downvote question

--- server/controllers/question.js
+++ server/controllers/question.js
@@ -40,7 +40,81 @@ class Controller {
     }
 
     static updateQuestion(req, res) {
-        res.send('masuk')
+        let update = {
+            title: req.body.title,
+            description: req.body.description
+        }
+
+        Question.updateOne({_id: req.params.id}, update)
+            .then(() => {
+                res.status(200).json({message: 'Question updated!'})
+            })
+            .catch(err => {
+                res.status(500).json({error: err.message})
+            })
+    }
+
+    static deleteQuestion(req, res) {
+        Question.deleteOne({_id: req.params.id})
+            .then(() => {
+                res.status(200).json({message: 'Question deleted!'})
+            })
+            .catch(err => {
+                res.status(500).json({error: err.message})
+            })
+    }
+
+    static upVote(req, res) {
+        Question.updateOne({_id: req.params.id}, {$pull: {downVote: req.decoded.id}})
+            .then(() => {
+                Question.updateOne({_id: req.params.id}, {$push: {upVote: req.decoded.id}})
+                    .then(() => {
+                        res.status(201).json({message: 'You just upvote this question!'})
+                    })
+                    .catch(err => {
+                        res.status(500).json({error: err.message})
+                    })
+            })
+            .catch(err => {
+                res.status(500).json({error: err.message})
+            })
+    }
+
+    static downVote(req, res) {
+        Question.updateOne({_id: req.params.id}, {$pull: {upVote: req.decoded.id}})
+            .then(() => {
+                Question.updateOne({_id: req.params.id}, {$push: {downVote: req.decoded.id}})
+                    .then(() => {
+                        res.status(201).json({message: 'You just down vote this question!'})
+                    })
+                    .catch(err => {
+                        res.status(500).json({error: err.message})
+                    })
+            })
+            .catch(err => {
+                res.status(500).json({error: err.message})
+            })
+    }
+
+    static getSpesificQuestion(req, res) {
+        Question.findById(req.params.id)
+            .populate('user', '_id name email')
+            .populate('upVote', '_id name email')
+            .populate('downVote', '_id name email')
+            // .populate({
+            //     path: 'answer',
+            //     select: '_id content user',
+            //     populate: {
+            //         path: 'user',
+            //         select: '_id name email'
+            //     }
+            // })
+            .then(question => {
+                res.status(200).json(question)
+            })
+            .catch(err => {
+                res.status(500).json({error: err.message})
+            })
     }
 
 }
---
+++ server/middleware/already.js
@@ -0,0 +1,31 @@
+const Question = require('../models/Question')
+const Answer = require('../models/Answer')
+
+module.exports = {
+    alreadyVoteQuestion(req, res, next) {
+        Question.findOne({_id: req.params.id, upVote: req.decoded.id})
+            .then(question => {
+                if (question) {
+                    res.status(401).json({error: 'You are not allowed to vote this question!'})
+                } else {
+                    next()
+                }
+            })
+            .catch(err => {
+                res.status(500).json({error: err.message})
+            })
+    },
+    alreadyDownVoteQuestion(req, res, next) {
+        Question.findOne({_id: req.params.id, downVote: req.decoded.id})
+            .then(question => {
+                if (question) {
+                    res.status(401).json({error: 'You are not allowed to down vote this question!'})
+                } else {
+                    next()
+                }
+            })
+            .catch(err => {
+                res.status(500).json({error: err.message})
+            })
+    }
+}
