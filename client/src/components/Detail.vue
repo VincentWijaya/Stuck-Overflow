6f92f12ad0ea5a8655f13fd435e26a46e3e47a92
Vincent Wijaya <weqqss@yahoo.com>
2018-09-25 16:30:25 +0700

    Finished all client

--- client/src/components/Detail.vue
+++ client/src/components/Detail.vue
@@ -24,15 +24,52 @@
       <div class="card-footer text-muted mb-4" v-for="answer in article.answer" :key="answer._id" v-if="isLoad">
         <h6>{{ answer.user.name }}: </h6>
         <p>{{ answer.content }}</p>
-        <button type="button" class="btn btn-xs btn-ligth" v-if="token && answer.isHim"><i class="fas fa-pencil-alt"></i></button>
+        <button type="button" class="btn btn-xs btn-ligth" data-toggle="modal" data-target="#editAnswerModal" v-if="token && answer.isHim" @click="setAnswerData(answer)"><i class="fas fa-pencil-alt"></i></button>
         &nbsp;
-        <button class="btn btn-sm btn-ligth" v-if="token && !answer.isHim"><i class="far fa-thumbs-up"></i></button> <i class="far fa-thumbs-up" v-else></i> ({{ answer.upVote.length }})
+        <button class="btn btn-sm btn-ligth" v-if="token && !answer.isHim" @click="upVoteAnswer(answer._id)"><i class="far fa-thumbs-up"></i></button> <i class="far fa-thumbs-up" v-else></i> ({{ answer.upVote.length }})
         &nbsp;
-        <button class="btn btn-sm btn-ligth" v-if="token && !answer.isHim"><i class="far fa-thumbs-down"></i></button> <i class="far fa-thumbs-down" v-else></i> ({{ answer.upVote.length }})
+        <button class="btn btn-sm btn-ligth" v-if="token && !answer.isHim" @click="downVoteAnswer(answer._id)"><i class="far fa-thumbs-down"></i></button> <i class="far fa-thumbs-down" v-else></i> ({{ answer.downVote.length }})
       </div>
 
       <div class="loader" v-if="!isLoad"></div>
 
+      <!-- Modal Edit Article -->
+      <div class="modal fade" id="editAnswerModal">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+              <h4 class="modal-title">Edit Answer</h4>
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+
+              <div class="container">
+                <form>
+                  <div class="form-group">
+                    <label for="title">Answer:</label>
+                    <div class="col-lg">
+                      <textarea class="form-control" rows="3" v-model="answerEditContent"></textarea>
+                    </div>
+                  </div>
+                </form>
+
+              </div>
+
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-dismiss="modal" @click="updateAnswer">Edit Answer</button>
+              <button type="button" class="btn btn-danger" data-dismiss="modal" @click="isLoadEdit = false">Close</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -49,7 +86,9 @@ export default {
       created: '',
       isLoad: false,
       answer: '',
-      hisQuestion: false
+      hisQuestion: false,
+      answerEditContent: '',
+      answerId: ''
     }
   },
   created () {
@@ -147,7 +186,7 @@ export default {
       let self = this
 
       axios({
-        method: 'get',
+        method: 'post',
         url: `${baseUrl}/answer/${id}`,
         headers: {
           token: this.token
@@ -159,9 +198,71 @@ export default {
         .then(() => {
           self.getArticle()
           self.$store.dispatch('getQuestion')
+          self.answer = ''
         })
         .catch(err => {
           console.log(err)
+        })
+    },
+    setAnswerData (data) {
+      this.answerEditContent = data.content
+      this.answerId = data._id
+    },
+    updateAnswer () {
+      let self = this
+
+      axios({
+        method: 'put',
+        url: `${baseUrl}/answer/${this.answerId}`,
+        headers: {
+          token: this.token
+        },
+        data: {
+          content: this.answerEditContent
+        }
+      })
+        .then(() => {
+          self.getArticle()
+          self.$store.dispatch('getQuestion')
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    upVoteAnswer (id) {
+      let self = this
+
+      axios({
+        method: 'patch',
+        url: `${baseUrl}/answer/upvote/${id}`,
+        headers: {
+          token: this.token
+        }
+      })
+        .then(() => {
+          self.getArticle()
+          self.$store.dispatch('getQuestion')
+        })
+        .catch(err => {
+          console.log(err.response)
+        })
+    },
+    downVoteAnswer (id) {
+      let self = this
+
+      axios({
+        method: 'patch',
+        url: `${baseUrl}/answer/downvote/${id}`,
+        headers: {
+          token: this.token
+        }
+      })
+        .then(() => {
+          self.getArticle()
+          self.$store.dispatch('getQuestion')
+        })
+        .catch(err => {
+          console.log(err.response)
         })
     }
   }// method
